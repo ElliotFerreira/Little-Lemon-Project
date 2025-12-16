b9ce1012c1841f3346d69f110cbcfe06a5993964
ElliotFerreira <elliotrafaielferreira@gmail.com>
2025-12-16 17:12:03 +0000

    Replace meaningless elements with semantic elements, create navbar, header, main and footer components and add in metadata with open graph protocol

---
+++ src/Footer.js
@@ -0,0 +1,36 @@
+export default function Footer(){
+
+    return (
+        <footer>
+            <nav>
+                <ul>
+                    <h3>Navigation</h3>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">About</a></li>
+                    <li><a href="/">Menu</a></li>
+                    <li><a href="/">Reservations</a></li>
+                    <li><a href="/">Order Online</a></li>
+                    <li><a href="/">Login</a></li>
+                </ul>
+            </nav>
+
+            <section>
+                <ul>
+                    <h3>Contact</h3>
+                    <li>Address</li>
+                    <li>Phone Number</li>
+                    <li>Email</li>
+                </ul>
+            </section>
+
+            <section>
+                <ul>
+                    <h3>Social Media Links</h3>
+                    <li>Address</li>
+                    <li>Phone Number</li>
+                    <li>Email</li>
+                </ul>
+            </section>         
+        </footer>
+    );
+}
---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import logo from "./images/logo.png"
+
+
+export default function Nav(){
+    return (
+        <nav>
+
+            <img src={logo} alt="Logo"></img>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Menu</a></li>
+                <li><a href="/">Reservations</a></li>
+                <li><a href="/">Order Online</a></li>
+                <li><a href="/">Login</a></li>
+            </ul>
+        </nav>
+    )
+}
